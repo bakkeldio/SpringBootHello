a7de18e295c224cdfa4ce4f94bd3f58a697f6042
bakkeldio <bakkeldi.orozbekov@iaau.edu.kg>
2020-01-28 14:34:19 +0600

    Initial commit

---
+++ src/test/java/com/example/demo/HelloWorldApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloWorldApplicationTests {
+
+
+
+}
